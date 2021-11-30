89bb7dbebe4a4cd150fb697b5c1175c3aa8dedd4
chandula_kavishka <wijesekerack.20@uom.lk>
2021-11-30 18:47:58 +0530

    still in development

---
+++ main.c
@@ -0,0 +1,47 @@
+/*
+ * Servo_motor.c
+ *
+ * Created: 11/30/2021 12:52:18 AM
+ * Author : chandula
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 8000000UL // 8 MHz clock speed
+#endif
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+void servo(unsigned char x);
+int main(void)
+{
+	while (1)
+	{
+		servo(1); //ultrasonic signal (1 = -90 ),(2 = 0 ),(3 = 90 )
+    }
+	
+void servo(unsigned char x)
+{
+	DDRD |= (1<<PD7);
+	TCNT1 = 0;
+	ICR1 = 2499;
+	TCCR2 = (1<<WGM11)|(1<<COM1A1);
+	TCCR2 = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+	
+	if(x==1){
+		OCR2 = 65;//if ultrasonic sensor give signal 1 servo goes to -90 digrees
+		_delay_ms(1500);
+	}
+	if(x==2){
+		OCR2 = 175;//if ultrasonic sensor give signal 2 servo goes to 0 digrees
+		_delay_ms(1500);
+	}
+	if(x==3){
+		OCR2 = 300;//if ultrasonic sensor give signal 13 servo goes to 90 digrees
+		_delay_ms(1500);
+	}
+	
+	
+	
+}
+
