eeaa42707ca333b94372d18f1fb033086538feea
chandulakavishka <95069010+chandulakavishka@users.noreply.github.com>
2022-05-14 23:07:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,36 +12,36 @@
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+
 void servo(unsigned char x);
 int main(void)
 {
 	while (1)
 	{
-		servo(1); //ultrasonic signal (1 = -90 ),(2 = 0 ),(3 = 90 )
-    }
-	
-void servo(unsigned char x)
-{
-	DDRD |= (1<<PD7);
-	TCNT1 = 0;
-	ICR1 = 2499;
-	TCCR2 = (1<<WGM11)|(1<<COM1A1);
-	TCCR2 = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+		         //ultrasonic signal (1 = -90 ),(2 = 0 ),(3 = 90 )  
+		servo(1);   
+	}          	
+}
+
+void servo(unsigned char x){
+	DDRD |= (1<<PD5);              /* Make OC1A pin as output */ 
+	TCNT1 = 0;                      /* Set timer1 count zero */
+	ICR1 = 2499;                   /* Set TOP count for timer1 in ICR1 register */
+	TCCR1A = (1<<WGM11)|(1<<COM1A1);       /* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
 	
 	if(x==1){
-		OCR2 = 65;//if ultrasonic sensor give signal 1 servo goes to -90 digrees
+		OCR1A = 65;//if ultrasonic sensor give signal 1 servo goes to -90 digrees
 		_delay_ms(1500);
 	}
 	if(x==2){
-		OCR2 = 175;//if ultrasonic sensor give signal 2 servo goes to 0 digrees
+		OCR1A = 175;//if ultrasonic sensor give signal 2 servo goes to 0 digrees
 		_delay_ms(1500);
 	}
 	if(x==3){
-		OCR2 = 300;//if ultrasonic sensor give signal 13 servo goes to 90 digrees
+		OCR1A = 300;//if ultrasonic sensor give signal 13 servo goes to 90 digrees
 		_delay_ms(1500);
-	}
-	
-	
-	
+	}	
 }
+
 
